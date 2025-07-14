a741f0007495fad2ba4442fc4efdbd39e030d79d
PSYkathi <akemsayeed@gmail.com>
2025-07-14 19:13:32 +0530

    js trial

---
+++ test.js
@@ -0,0 +1,20 @@
+console.log("hi")
+
+var example3 = "hi";
+console.log(example3)
+var example3;
+var example3 = "hello";
+console.log(example3)
+
+
+let example;
+example = "kozhikode";
+console.log(example);
+example = "kannur";
+console.log(example);
+
+
+
+
+const example2 = 2;
+console.log(example2)
